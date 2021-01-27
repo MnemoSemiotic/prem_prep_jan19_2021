216ecce12a2ccfcbb326d40b19c7b991193352ca
Clark <clark@tsnes-MBP.domain>
2021-01-26 18:55:17 -0700

    added day 6

--- src/python/main.py
+++ src/python/main.py
@@ -1,12 +1,13 @@
 
-
 def main():
     print("\n" * 20)
     # import day_1_lib
     # import day_2_lib
     # import day_3_lib
     # import day_4_lib
-    import day_5_lib
+    # import day_5_lib
+    import day_6_lib
+
 
 if __name__ == "__main__":
     main()
