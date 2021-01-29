9798a905767dd2cef93921a66c9d34f0105c3947
Tovio Roberts <clownfragment@gmail.com>
2021-01-28 19:35:19 -0700

    Auto Updated: Thu 35:19 Jan 28 2021

--- src/stats/01_summary_stats/summary_stats.py
+++ src/stats/01_summary_stats/summary_stats.py
@@ -1,3 +1,16 @@
 
-def mean(lst):
-    sum(lst / len(lst))
+# def mean(lst):
+#     sum(lst / len(lst))
+
+def mean(lst, trim_by=0):
+    lst_ = lst.copy()
+
+    if trim_by > 0:
+        lst_ = sorted(lst_)[trim_by:-trim_by]
+
+    return sum(lst_ / len(lst_))
+
+a = [1, 5, 7, 10, 15, 23, 35, 67, 220, 2000]
+
+mean(a, trim_by=0)
+mean(a, trim_by=1)
