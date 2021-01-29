16782f7cd6aac11635b8b3109b9f64be370fcc6a
Tovio Roberts <clownfragment@gmail.com>
2021-01-28 19:46:55 -0700

    Auto Updated: Thu 46:19 Jan 28 2021

--- src/stats/01_summary_stats/summary_stats.py
+++ src/stats/01_summary_stats/summary_stats.py
@@ -17,3 +17,20 @@ a = [1, 5, 7, 10, 15, 23, 35, 67, 220, 2000]
 # print(mean(a, trim_by=2)) # 26.167
 
 
+
+'''
+Breakout
+
+An article published in the journal, Indoor Air, considered two different air samples to 
+test for endotoxin concentrations, the first in urban households, and the second in farmhouses.
+Urban: 6.0, 5.0, 11.0, 33.0, 4.0, 5.0, 80.0, 18.0, 35.0, 17.0, 23.0
+Farmhouse: 4.0, 14.0, 11.0, 9.0, 9.0, 8.0, 4.0, 20.0, 5.0, 8.9, 21.0, 9.2, 3.0, 2.0, 0.3
+'''
+
+urban = [6.0, 5.0, 11.0, 33.0, 4.0, 5.0, 80.0, 18.0, 35.0, 17.0, 23.0]
+farmhouse = [4.0, 14.0, 11.0, 9.0, 9.0, 8.0, 4.0, 20.0, 5.0, 8.9, 21.0, 9.2, 3.0, 2.0, 0.3]
+
+'''
+A. Determine the sample mean for each group.
+'''
+print(f'Sample Mean Urban: {mean(urban)}')
