1efc9e911a3781c857e10834a021bfde4ecc2a7f
Tovio Roberts <clownfragment@gmail.com>
2021-01-28 20:03:45 -0700

    Auto Updated: Thu 03:20 Jan 28 2021

--- src/stats/01_summary_stats/summary_stats.py
+++ src/stats/01_summary_stats/summary_stats.py
@@ -50,5 +50,12 @@ from each group.
 Median
 '''
 
+def median(lst):
+
 odd_list = [13, 18, 13, 14, 13, 16, 14, 21, 13]
 even_list = [15, 14, 10, 8, 12, 8, 16, 13]
+
+print(sorted(odd_list))
+
+
+print(sorted(even_list))
