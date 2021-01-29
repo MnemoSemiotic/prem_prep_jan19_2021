60d85f1f0a39be30a2f68bd9118401b9c1515735
Tovio Roberts <clownfragment@gmail.com>
2021-01-28 20:52:00 -0700

    Auto Updated: Thu 52:20 Jan 28 2021

--- src/stats/01_summary_stats/summary_stats.py
+++ src/stats/01_summary_stats/summary_stats.py
@@ -127,3 +127,21 @@ Do you think mean or median is a “better” measure of center for this data? w
 
 
 
+'''
+Mode
+'''
+
+def mode(lst):
+    most_occurring = lst[0]
+
+    for item in lst[1:]:
+        if lst.count(item) > lst.count(most_occurring):
+            most_occurring = item
+
+    return most_occurring
+
+mode_lst = ['kangaroo', 'muskrat', 'platypus', 'muskrat', 'squid', 'squirrel', 'muskrat']
+
+# print(mode(mode_lst))
+
+
