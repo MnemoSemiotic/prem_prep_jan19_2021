600982e043e7cedaa345fbf48ee3cca0c4a62a5d
Tovio Roberts <clownfragment@gmail.com>
2021-02-01 19:37:47 -0700

    Auto Updated: Mon 37:19 Feb 01 2021

--- src/stats/01_summary_stats/summary_stats.py
+++ src/stats/01_summary_stats/summary_stats.py
@@ -175,3 +175,23 @@ def five_number_summary(lst):
     min_ = min(lst)
     max_ = max(lst)
     med = median(lst)
+
+    sorted_lst = sorted(lst)
+
+    if len(lst) % 2 == 1:
+        lower_half = sorted_lst[0: int(len(lst) / 2)]
+        upper_half = sorted_lst[int(len(lst) / 2) + 1: ]
+    
+
+    q1 = median(lower_half)
+    q3 = median(upper_half)
+
+    return min_, q1, med, q3, max_
+
+
+a = [15,2,9,5,6,7,27,12,18,19,1]
+b = [6,1,4,51,7,16,10,14,46,22,24,56,48,54]
+
+
+print(sorted(a))
+print(five_number_summary(a))
