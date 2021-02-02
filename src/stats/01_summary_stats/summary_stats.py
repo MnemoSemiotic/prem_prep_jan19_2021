8d919b54e613166a5a522118ae5e1e198a277ae9
Tovio Roberts <clownfragment@gmail.com>
2021-02-01 19:54:19 -0700

    Auto Updated: Mon 54:19 Feb 01 2021

--- src/stats/01_summary_stats/summary_stats.py
+++ src/stats/01_summary_stats/summary_stats.py
@@ -210,3 +210,13 @@ def iqr(lst):
 
 a = list(range(0, 50+1, 5)) 
 b = list(range(0, 100+1, 5))
+
+print(a)
+print(five_number_summary(a))
+print(iqr(a))
+
+print()
+
+print(b)
+print(five_number_summary(b))
+print(iqr(b))
