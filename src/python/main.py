4edd669a7a6b2e4f17cb779e65599d98ba62eeab
Clark <clark@tsnes-MBP.domain>
2021-02-04 18:57:34 -0700

    update

--- src/python/main.py
+++ src/python/main.py
@@ -9,7 +9,8 @@ def main():
     # import day_06_lib
     # import day_07_lib
     # import day_08_lib
-    import day_9_lib
+    # import day_09_lib
+    import day_10_lib
 
 
 if __name__ == "__main__":
