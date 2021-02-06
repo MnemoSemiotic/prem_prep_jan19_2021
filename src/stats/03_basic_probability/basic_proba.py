4b36d07d9dd56b936c1e9fbac5eb5a5044b8e0fd
Tovio Roberts <clownfragment@gmail.com>
2021-02-06 13:36:54 -0700

    Auto Updated: Sat 36:13 Feb 06 2021

--- src/stats/03_basic_probability/basic_proba.py
+++ src/stats/03_basic_probability/basic_proba.py
@@ -25,4 +25,24 @@ for pet_outcome in pet_outcomes:
 # print(len(two_plus_cats) / len(pet_outcomes))
 
 
+'''
+Write a function called series_of_flips.
 
+define one parameter, n, which represents the number of coin flips
+return a list of length n containing n random coin flips
+
+'''
+from random import choice
+
+def coin_flip():
+    flip = ['H', 'T']
+    return choice(flip)
+
+def series_of_flips(n):
+    return [coin_flip() for _ in range(n)]
+    # flips = []
+    # for _ in range(n):
+    #     flips.append(coin_flip())
+    # return flips
+
+print(series_of_flips(4))
