fdda5de41a84e8d321c94c264080186e7a313aba
Tovio Roberts <clownfragment@gmail.com>
2021-02-09 19:38:26 -0700

    Auto Updated: Tue 38:19 Feb 09 2021

--- src/stats/05_general_analytic_approach/analytic_approach.py
+++ src/stats/05_general_analytic_approach/analytic_approach.py
@@ -47,4 +47,10 @@ for outcome in S:
     if outcome[0] >= range_to_hit and outcome[2].count('H') == 2:
         hits.append(outcome)
 
-print(f'proba: {round(len(hits) / len(S), 3)}')
+# print(f'proba: {round(len(hits) / len(S), 3)}')
+
+
+
+'''
+Random Sampling
+'''
