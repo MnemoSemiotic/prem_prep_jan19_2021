d8987e6f719fe8591a9641dfe65bc2c73d20e63d
Tovio Roberts <clownfragment@gmail.com>
2021-02-09 19:57:20 -0700

    Auto Updated: Tue 57:19 Feb 09 2021

--- src/stats/05_general_analytic_approach/analytic_approach.py
+++ src/stats/05_general_analytic_approach/analytic_approach.py
@@ -76,5 +76,14 @@ for _ in range(100000):
         outcomes[attending] = 0
     outcomes[attending] += 1
 
-for k, v in sorted(outcomes.items()):
-    print(f'{k}: {v}')
+# for k, v in sorted(outcomes.items()):
+#     print(f'{k}: {v}')
+
+
+eighty_to_ninety = 0
+total = sum(outcomes.values())
+
+for attendees in range(80, 90+1):
+    eighty_to_ninety += outcomes[attendees]
+
+print(f'{round(eighty_to_ninety/total, 3)}')
