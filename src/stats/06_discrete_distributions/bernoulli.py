67accaf169972d02c01a5471a66564f9e4116c5b
Tovio Roberts <clownfragment@gmail.com>
2021-02-10 19:32:35 -0700

    Auto Updated: Wed 32:19 Feb 10 2021

--- src/stats/06_discrete_distributions/bernoulli.py
+++ src/stats/06_discrete_distributions/bernoulli.py
@@ -11,5 +11,7 @@ def bernoulli(p_success=0.5):
 # print(bernoulli(p_success=0.3))
 
 
-trials = 10000000
-print([bernoulli(p_success=0.3) for _ in range(trials)].count(True) / trials)
+# trials = 10000000
+# print([bernoulli(p_success=0.3) for _ in range(trials)].count(True) / trials)
+
+
