78df11e92d265683a29ed1ab573aa314e3e135f1
Tovio Roberts <clownfragment@gmail.com>
2021-02-10 20:39:43 -0700

    Auto Updated: Wed 39:20 Feb 10 2021

--- src/stats/06_discrete_distributions/binomial.py
+++ src/stats/06_discrete_distributions/binomial.py
@@ -77,4 +77,14 @@ Binomial through sampling
 '''
 
 
+def get_bit():
+    return choice([0,1])
 
+def generate_n_bits(n=8):
+    lst = []
+
+    for _ in range(n):
+        lst.append(get_bit())
+    return lst
+
+print(generate_n_bits(12))
