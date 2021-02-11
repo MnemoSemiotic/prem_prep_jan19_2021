093b8755ed8dcc3e9624cbbd80e4c863891c9336
Tovio Roberts <clownfragment@gmail.com>
2021-02-10 19:44:17 -0700

    Auto Updated: Wed 44:19 Feb 10 2021

---
+++ src/stats/06_discrete_distributions/binomial.py
@@ -0,0 +1,16 @@
+
+
+def gen_4_bit_binary():
+    bin_dct = dict()
+    decimal = 0
+
+    for i in range(2):
+        for j in range(2):
+            for k in range(2):
+                for l in range(2):
+                    bin_dct[decimal] = [i,j,k,l]
+                    decimal += 1
+    return bin_dct
+
+for dec, bin_ in gen_4_bit_binary().items():
+    print(f'{dec}: {bin_}')
