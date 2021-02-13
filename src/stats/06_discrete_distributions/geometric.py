fde35db90d006322a2906f3f78d3c1eb4e9a11f9
Tovio Roberts <clownfragment@gmail.com>
2021-02-13 13:18:48 -0700

    Auto Updated: Sat 18:13 Feb 13 2021

---
+++ src/stats/06_discrete_distributions/geometric.py
@@ -0,0 +1,9 @@
+
+def geometric_pmf(p, k, inclusive=True):
+    return p * (1-p)**(k-inclusive)
+    # if inclusive:
+    #     return p * (1-p)**(k-1)
+    # else:
+    #     return p * (1-p)**(k)
+
+
