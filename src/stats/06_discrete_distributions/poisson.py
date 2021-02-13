9e949bb80cd2a9310b3dccfd48d0db86364ab286
Tovio Roberts <clownfragment@gmail.com>
2021-02-13 12:11:38 -0700

    Auto Updated: Sat 11:12 Feb 13 2021

--- src/stats/06_discrete_distributions/poisson.py
+++ src/stats/06_discrete_distributions/poisson.py
@@ -8,3 +8,20 @@ def poisson_pmf(lmbda, k):
 # print(poisson_pmf(10, 10))
 
 
+def combinations(n, k):
+    return int(factorial(n) / (factorial(n-k) * factorial(k)))
+
+def binomial_pmf(n, k, p=0.5):
+    return combinations(n, k) * (p**k) * (1-p)**(n-k)
+
+
+
+lmbda = 10
+k = 10
+
+for n in range(k, 10000):
+    print(f'binom: {round(binomial_pmf(n, k, p=(lmbda/n)), 7)}')
+    print(f'binom: {round(poisson_pmf(lmbda, k)}')
+
+
+
