c0d61676f793a58569540241ae1547137cf30a90
Tovio Roberts <clownfragment@gmail.com>
2021-02-17 20:00:08 -0700

    Auto Updated: Wed 00:20 Feb 17 2021

--- src/stats/07_continuous_distributions/exponential.py
+++ src/stats/07_continuous_distributions/exponential.py
@@ -30,11 +30,14 @@ Suppose you’re on a street corner trying to hail a taxi cab. Let
 X = The amount of time (in minutes) that you have to wait.
 X ~ exponential(0.1)
 ƛ = 0.1 (meaning the rate of cab arrivals is 0.1 per minute)
-
-What’s the probability that you’ll have to wait more than ten minutes?
-What is the amount of time that you would expect to wait?
-What is the variance of this random variable?
-
 '''
 
 lmbda = 0.1
+
+# What’s the probability that you’ll have to wait more than ten minutes?
+print(1 - exponential_cdf(lmbda, x=10))
+
+# What is the amount of time that you would expect to wait?
+
+
+# What is the variance of this random variable?
