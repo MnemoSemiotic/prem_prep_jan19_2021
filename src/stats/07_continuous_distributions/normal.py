7d4df509003112ac7f1499e5b8a5ead330e50924
Tovio Roberts <clownfragment@gmail.com>
2021-02-17 21:09:41 -0700

    Auto Updated: Wed 09:21 Feb 17 2021

--- src/stats/07_continuous_distributions/normal.py
+++ src/stats/07_continuous_distributions/normal.py
@@ -11,11 +11,11 @@ def normal_cdf(x=0, mu=0, sigma=1):
     accum = 0.0
 
     for val in x_vals:
-        accum += normal_pdf(val, mu, sigma)
+        accum += (val + 0.001) * normal_pdf(val, mu, sigma)
         if val > x:
             break
 
-    return accum * 0.001
+    return accum
 
 
 # print(normal_cdf(x=300, mu=475, sigma=98)) # 0.0370
