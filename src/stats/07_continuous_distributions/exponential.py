ff05cf63d643e2b322570bc55e2402484d017d3f
Tovio Roberts <clownfragment@gmail.com>
2021-02-17 19:51:25 -0700

    Auto Updated: Wed 51:19 Feb 17 2021

--- src/stats/07_continuous_distributions/exponential.py
+++ src/stats/07_continuous_distributions/exponential.py
@@ -1,14 +1,14 @@
 from math import e, sqrt
 
 
- def exponential_pdf(lmbda, x):
-     if x < 0: return 0
-     return lmbda * e**(-lmbda * x)
+def exponential_pdf(lmbda, x):
+    if x < 0: return 0
+    return lmbda * e**(-lmbda * x)
 
 
- def exponential_cdf(lmbda, x):
-     if x < 0: return 0
-     return 1 - e**(-lmbda * x)
+def exponential_cdf(lmbda, x):
+    if x < 0: return 0
+    return 1 - e**(-lmbda * x)
 
 
 def exponential_mean(lmbda):
